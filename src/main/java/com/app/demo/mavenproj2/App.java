929ad44d3a9361d9116b4841f27855f59d852916
mani <koremani024@gmail.com>
2023-12-14 15:17:20 +0530

    demo

---
+++ src/main/java/com/app/demo/mavenproj2/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.mavenproj2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Mani" );
+    }
+}
